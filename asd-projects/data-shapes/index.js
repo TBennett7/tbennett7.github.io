0549f486941e6e2abc06e7ff22cad25afc242149
TBennett7 <trinity.bennett@jpschools.org>
2025-06-09 16:09:15 +0000

    completed data shapes project

--- asd-projects/data-shapes/index.js
+++ asd-projects/data-shapes/index.js
@@ -42,12 +42,12 @@ $(document).ready(function () {
   }
 dataShapes.push(shape)
   // TODO 2: add a new property to all data shapes
-  for (var i = 0; i <= dataShapes.length; i++){
+  for (var i = 0; i <= dataShapes.length - 1; i++){
     var currentShape = dataShapes[i];
     if (currentShape.color === "red"){
       currentShape.goodBehavior = "bounce";
     }
-    else if (shape.color === "blue"){
+    else if (currentShape.color === "blue"){
       currentShape.goodBehavior = "blink";
     }
     else {
@@ -62,10 +62,17 @@ dataShapes.push(shape)
   }
 
   // TODO 4-a: add a function that handles the good display type
-  
+    function handleGood(color, shape, repeat){
+      setBackgroundWithSimple(color, shape, repeat)
+      animationDetails.displayType = 2
+    }
 
   // TODO 5-a: add a function that handles the bad display type
-  
+    function handleBad(data, repeat){
+      repeat++
+      setBackgroundWithMixed(data, repeat)
+      animationDetails.displayType = 3
+    }
 
   /////////////////////////////////////////////////
   // BUTTON HANDLERS BELOW HERE (3-b, 4-b, 5-b) ///
@@ -73,17 +80,20 @@ dataShapes.push(shape)
 
   function staticDisplay() {
     // TODO 3-b: call your handleStatic function
-    handleStatic(currentShape)
+    handleStatic(dataShapes[currentIndex])
   }
 
   function goodDisplay() {
     // TODO 4-b: call your handleGood function
-    
+    var currentShape = dataShapes[currentIndex]
+    handleGood(currentShape.color, currentShape.shape, currentShape.repeat)
   }
 
   function badDisplay() {
     // TODO 5-b: call your handleBad function
-    
+    currentShape = dataShapes[currentIndex]
+    var repeat = currentShape.repeat
+    handleBad(currentShape, repeat)
   }
 
   /////////////////////////////////////////////////
